197b2b4f8eb80625172e8a6153da9dd05df52d95
RyanG-O <ryangong07@gmail.com>
2026-09-09 22:08:20 +0800

    configured webpack and created js files

---
+++ src/todo.js
@@ -0,0 +1,13 @@
+class Todo {
+    constructor(title, description, dueDate, priority){
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+export default class TodoList {
+    
+}
