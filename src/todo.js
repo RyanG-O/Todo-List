32939cf3d5e41bef6a9db1302020975530d9dd95
RyanG-O <ryangong07@gmail.com>
2026-09-14 21:21:45 +0800

    created basic structure

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,5 @@
+import {compareAsc, format} from "date-fns";
+
 class Todo {
     constructor(title, description, dueDate, priority){
         this.id = crypto.randomUUID();
@@ -9,5 +11,23 @@ class Todo {
 }
 
 export default class TodoList {
-    
+    constructor(){
+        this.list = [];
+    }
+
+    addTodo(todo){
+        this.list.push(todo);
+    }
+
+    deleteTodo(id){
+        this.list = this.list.filter((element) => element.id !== id);
+    }
+
+    sortPriority(){
+        this.list.sort((a, b) => a.priority - b.priority);
+    }
+
+    sortDate(){
+        this.list.sort(compareAsc);
+    }
 }
